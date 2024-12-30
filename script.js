925b9f3f3e3c87261200bc8bbb2f212712ad80a0
Nishita-codes <nishitapatidar03@gmail.com>
2024-12-31 01:35:09 +0530

    added files

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const apikey = "89bde5b169909b936b5c20e45d685793";
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=germany&units=metric";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
-async function checkweather(){
-    const response = await fetch(apiURL + `&appid=${apikey}`);
+const searchBox = document.querySelector(".search input");
+const searchbtn = document.querySelector(".search button");
+async function checkweather(city) {
+    const response = await fetch(apiURL + city + `&appid=${apikey}`);
     var data = await response.json();
     console.log(data);
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = parseInt(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".Wind").innerHTML = data.wind.speed + "km/h";
 }
-checkweather();
 
-document.querySelector(".city").innerHTML = data.name;
-document.querySelector(".temp").innerHTML = data.main.temp;
-document.querySelector(".humidity").innerHTML = data.main.humidity;
-document.querySelector(".Wind").innerHTML = data.wind.speed;
+searchbtn.addEventListener("click" , ()=>{
+    checkweather(searchBox.value);
+})
+
+
